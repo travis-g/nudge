a9c03ebe9c8dd91fb634955b7f6e496aabfcf3e2
Travis Grammer <travisjgrammer@gmail.com>
2018-10-16 08:58:22 -0400

    allow key+modifier combinations

--- nudge.go
+++ nudge.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -19,8 +20,8 @@ var (
 	flagKey      string
 )
 
-// Nudge keeps the computer awake by pressing a provided key. It's simply a
-// wrapper for `robotgo.KeyTap()`.
+// Nudge keeps the computer awake by pressing a provided key combination. It's
+// simply a wrapper for `robotgo.KeyTap()`.
 func Nudge(key string, modifiers interface{}) {
 	robotgo.KeyTap(key, modifiers)
 }
@@ -34,12 +35,26 @@ func log(s string, file *os.File) {
 func parseFlags() {
 	flag.BoolVar(&flagDisabled, "disable", false, "start disabled")
 	flag.DurationVar(&flagInterval, "interval", 59*time.Second, "interval between nudges")
-	flag.StringVar(&flagKey, "key", "f16", "key to press")
+	flag.StringVar(&flagKey, "key", "f16", "key(s) to press")
 	flag.Parse()
+}
+
+// Parses a key combination in the format `key+mod...` into KeyTap components
+func parseKeys(flag string) (string, interface{}) {
+	keys := strings.Split(strings.ToLower(flag), "+")
+	switch count := len(keys); count {
+	case 0:
+		return "f16", nil
+	case 1:
+		return keys[0], nil
+	default:
+		return keys[0], keys[1:]
+	}
 }
 
 func main() {
 	parseFlags()
+
 	PID = os.Getpid()
 	log(fmt.Sprintf("pid: %d", PID), os.Stderr)
 
@@ -47,14 +62,14 @@ func main() {
 	sigQuit := make(chan os.Signal, 1)
 	signal.Notify(sigQuit, syscall.SIGINT, syscall.SIGTERM)
 
-	// Setup toggling of disabled status via SIGUSR1:
+	// Setup toggling via SIGUSR1:
 	sigToggle := make(chan os.Signal, 0)
 	signal.Notify(sigToggle, syscall.SIGUSR1)
 	go func() {
 		// Set up an infinite loop to continually listen for SIGUSR1 and toggle
 		// once per signal:
 		for {
-			<-sigToggle
+			<-sigToggle // blocks until sigToggle received
 			flagDisabled = !flagDisabled
 			log(fmt.Sprintf("disabled: %t", flagDisabled), os.Stdout)
 		}
@@ -64,10 +79,11 @@ func main() {
 	nudger := time.NewTicker(flagInterval)
 	defer nudger.Stop()
 	go func() {
+		key, modifiers := parseKeys(flagKey)
 		// Nudge at every interval:
 		for t := range nudger.C {
 			if !flagDisabled {
-				Nudge(flagKey, nil)
+				Nudge(key, modifiers)
 				log(fmt.Sprintf("nudged: %v", t.Local().Format("15:04:05")), os.Stderr)
 			}
 		}
